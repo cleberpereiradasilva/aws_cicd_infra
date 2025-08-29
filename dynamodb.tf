4b4d659da26f521ec65ed23ff1b67d6301cf90da
Cleber Silva <cleber_pesilva@yahoo.com.br>
2025-08-29 20:07:46 -0300

    pipeline configure

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,36 +1,63 @@
-resource "aws_dynamodb_table" "clients" {
-  provider = var.stage == "prd" ? aws.prd : aws.dev
-  name           = "${local.prefix}Clients_${var.version}"
+variable "stage" {
+  type    = string
+  default = "dev"
+}
+
+provider "aws" {
+  alias  = "dev"
+  region = var.stage == "dev" ? "sa-east-1" : ""
+}
+
+provider "aws" {
+  alias  = "prd"
+  region = var.stage == "prd" ? "sa-east-1" : ""
+}
+
+resource "aws_dynamodb_table" "clients_dev" {
+  provider = aws.dev
+  count    = var.stage == "dev" ? 1 : 0
+
+  name           = "dev-Clients_v1"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "ID"
-
   attribute {
     name = "ID"
     type = "S"
   }
-
-  dynamic "attribute" {
-    for_each = [ { name="CNPJ", type="S" } ]
-    content {
-      name = attribute.value.name
-      type = attribute.value.type
-    }
-  }
-
   global_secondary_index {
-    name               = "${local.prefix}Clients_${var.version}-CNPJIndex"
+    name               = "dev-Clients_v1-CNPJIndex"
     hash_key           = "CNPJ"
     projection_type    = "ALL"
   }
-
-  lifecycle {
-    prevent_destroy = var.stage == "prd"
-  }
-
   tags = {
-    Environment = var.stage
+    Environment = "dev"
     Project     = "MyProject"
-    Version     = var.version
+    Version     = "v1"
   }
 }
+
+resource "aws_dynamodb_table" "clients_prd" {
+  provider = aws.prd
+  count    = var.stage == "prd" ? 1 : 0
+
+  name           = "prd-Clients_v1"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "ID"
+  attribute {
+    name = "ID"
+    type = "S"
+  }
+  global_secondary_index {
+    name               = "prd-Clients_v1-CNPJIndex"
+    hash_key           = "CNPJ"
+    projection_type    = "ALL"
+  }
+  tags = {
+    Environment = "prd"
+    Project     = "MyProject"
+    Version     = "v1"
+  }
+}
+
+
 
