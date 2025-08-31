a14b9f8af7304f933d0803ba98773400f5a52abe
Cleber Silva <cleber_pesilva@yahoo.com.br>
2025-08-31 12:08:00 -0300

    pipeline configure

--- main.tf
+++ main.tf
@@ -2,16 +2,6 @@ terraform {
   backend "s3" {}
 }
 
-module "dynamodb" {
-  source = "./dynamodb"
-  stage  = var.stage
-  aws_region = var.aws_region
-  aws_access_key = var.aws_access_key
-  aws_secret_key = var.aws_secret_key
-  s3_bucket = var.s3_bucket
-}
-
-
 module "documents_bucket" {
   source         = "./bucket"
   stage          = var.stage
@@ -26,4 +16,13 @@ module "cloudfront" {
   stage          = var.stage
   aws_region     = var.aws_region
   bucket_name    = module.documents_bucket.bucket_name
+}
+
+module "dynamodb" {
+  source = "./dynamodb"
+  stage  = var.stage
+  aws_region = var.aws_region
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+  s3_bucket = var.s3_bucket
 }
