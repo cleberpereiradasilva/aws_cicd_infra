34118e86b5737a32366f7995cb62acef73070379
Cleber Silva <cleber_pesilva@yahoo.com.br>
2025-08-31 01:28:52 -0300

    pipeline configure

--- bucket/documents.tf
+++ bucket/documents.tf
@@ -1,19 +1,20 @@
 resource "aws_s3_bucket" "documents" {
   bucket = "${terraform.workspace}-insurance-documents-${var.bucket_suffix}"
-
-  acl = "private"
+  acl    = "private"
 
   tags = {
     Environment = terraform.workspace
     Project     = "Insurance Project"
   }
-
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
-
 }
+
+resource "aws_s3_bucket_public_access_block" "documents_block" {
+  bucket = aws_s3_bucket.documents.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 
--- bucket/web-files.tf
+++ bucket/web-files.tf
@@ -1,38 +1,39 @@
 resource "aws_s3_bucket" "web-files" {
   bucket = "${terraform.workspace}-insurance-web-files-${var.bucket_suffix}"
-
-
-  acl = "private"
+  acl    = "private"
 
   tags = {
     Environment = terraform.workspace
     Project     = "Insurance Project"
   }
+}
 
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
+resource "aws_s3_bucket_public_access_block" "web_files_block" {
+  bucket = aws_s3_bucket.web-files.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket" "web-files-backups" {
   bucket = "${terraform.workspace}-insurance-web-files-backups-${var.bucket_suffix}"
-
-  acl = "private"
+  acl    = "private"
 
   tags = {
     Environment = terraform.workspace
     Project     = "Insurance Project"
   }
+}
 
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
+resource "aws_s3_bucket_public_access_block" "web_files_backups_block" {
+  bucket = aws_s3_bucket.web-files-backups.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 
