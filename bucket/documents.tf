6214f277cce363c333a0bf1f2beace127a2d87f8
Cleber Silva <cleber_pesilva@yahoo.com.br>
2025-08-31 01:25:15 -0300

    pipeline configure

--- bucket/documents.tf
+++ bucket/documents.tf
@@ -1,9 +1,19 @@
 resource "aws_s3_bucket" "documents" {
   bucket = "${terraform.workspace}-insurance-documents-${var.bucket_suffix}"
 
+  acl = "private"
+
   tags = {
     Environment = terraform.workspace
     Project     = "Insurance Project"
   }
+
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+
 }
 
--- bucket/web-files.tf
+++ bucket/web-files.tf
@@ -1,18 +1,37 @@
 resource "aws_s3_bucket" "web-files" {
   bucket = "${terraform.workspace}-insurance-web-files-${var.bucket_suffix}"
 
+
+  acl = "private"
+
   tags = {
     Environment = terraform.workspace
     Project     = "Insurance Project"
+  }
+
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
   }
 }
 
 resource "aws_s3_bucket" "web-files-backups" {
   bucket = "${terraform.workspace}-insurance-web-files-backups-${var.bucket_suffix}"
 
+  acl = "private"
+
   tags = {
     Environment = terraform.workspace
     Project     = "Insurance Project"
+  }
+
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
   }
 }
 
