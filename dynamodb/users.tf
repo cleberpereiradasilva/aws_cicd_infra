080b151336c38c4523960ffca2639f096c79281f
Cleber Silva <cleber_pesilva@yahoo.com.br>
2025-09-13 23:58:05 -0300

    create dynamo user table

--- dynamodb/users.tf
+++ dynamodb/users.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "users" {
   name         = "${terraform.workspace}-users"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     = "uid"
 
   attribute {
-    name = "id"
+    name = "uid"
     type = "S"
   }
 
